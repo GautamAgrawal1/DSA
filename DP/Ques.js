cc41a60920595433ff9ca3a50cbbb40d89d5c75d
GAUTAM AGRAWAL <gautamagrawal@GAUTAMs-MacBook-Air.local>
2026-02-24 22:00:03 +0530

    Added latest changes

--- DP/Ques.js
+++ DP/Ques.js
@@ -1166,7 +1166,40 @@ class Solution {
 
 
 
+//Regular expression matching
 
+class Solution {
+    public boolean isMatch(String s, String p) {
+        int l1=p.length();
+        int l2=s.length();
+
+        boolean [][]dp=new boolean[l1+1][l2+1];
+        dp[0][0]=true;
+        for(int i=2;i<=l1;i++){
+            if(p.charAt(i-1)=='*'){
+                dp[i][0]=dp[i-2][0];
+            }
+        }
+        for(int i=1;i<=l1;i++){
+            for(int j=1;j<=l2;j++){
+                if(s.charAt(j-1)==p.charAt(i-1) || p.charAt(i-1)=='.'){
+                    dp[i][j]=dp[i-1][j-1];
+                }
+                else if(p.charAt(i-1)=='*'){
+                    dp[i][j]=dp[i-2][j];
+
+                    if(p.charAt(i-2)==s.charAt(j-1) || p.charAt(i-2)=='.'){
+                        dp[i][j]=dp[i][j] || dp[i][j-1];
+                    }
+                }
+                else{
+                    dp[i][j]=false;
+                }
+            }
+        }
+        return dp[l1][l2];
+    }
+}
 
 
 
